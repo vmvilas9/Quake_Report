f91732e7a5ba9abf234f1cca1a956276ba5ec957
Vilas <vmvilas9@gmail.com>
2018-03-07 00:43:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,31 @@
+package com.example.android.quakereport;
+
+/**
+ * Created by Admin on 06-03-2018.
+ */
+
+public class Earthquake {
+
+    private double mMagnitude;
+
+    private String mLocation;
+
+    private long mTimeInMilliseconds;
+
+    private String mUrl;
+
+    public Earthquake(double magnitude,String location,long timeInMilliseconds,String url){
+        mMagnitude = magnitude;
+        mLocation = location;
+        mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
+    }
+
+    public double getMagnitude(){return mMagnitude;}
+
+    public String getLocation(){return mLocation;}
+
+    public long getTimeInMilliseconds(){return mTimeInMilliseconds;}
+
+    public String getUrl(){return mUrl;}
+}
